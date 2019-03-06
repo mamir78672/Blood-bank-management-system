84de3e307d83d2e41ed22350bd9bc35d0e9a2faa
Muhammad Amir <48288115+mamir78672@users.noreply.github.com>
2019-03-06 22:13:21 +0500

    Add files via upload
    This project is developed to manage the blood stock in the "BLOOD BANK" and the blood packets are maintained in the database. New blood details are entered in to the project to manage blood details. Blood donor details are entered and maintained in the database. Blood sales and blood purchase are entered and maintained in this project. Blood stock reports, sales reports and blood purchase reports are managed in this project.

---
+++ JavaApplication1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Rizwan Qureshi
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        Mainpage form2=new Mainpage();
+        form2.setVisible(true);
+        
+      //   Login form3=new Login();
+        //form3.setVisible(true);
+        
+        //Packet form3=new Packet();
+        //form3.setVisible(true);
+        
+        //Search form4=new Search();
+        //form4.setVisible(true);
+        
+        //Sell form5=new Sell();
+        //form5.setVisible(true);
+        
+        //Buyer form6=new Buyer();
+        //form6.setVisible(true);
+        
+        //D_Detail form7=new D_Detail();
+        //form7.setVisible(true);
+        
+        //S_Detail form8=new S_Detail();
+        //form8.setVisible(true);
+        
+    }
+    
+}
